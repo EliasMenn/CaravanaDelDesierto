998e3fff0e9df48a9744e8804cb3ae5791b03155
Agusjaime <agustinjaime0804@gmail.com>
2026-04-27 14:18:36 -0300

    implementacion de operaciones al final de la lista
    cree las primitivas de insertar al final y las codifique, ademas agregue las firmas de operaciones de consulta, de ordenamiento y de eliminacion (que luego codificaré

--- headers/ListaDoEnCi.h
+++ headers/ListaDoEnCi.h
@@ -19,4 +19,15 @@ void crearLista(tListaDobCirc *pListaDobCirc);
 //Operaciones al principio
 int insPrimListDobCirc(tListaDobCirc *pLista, void *dato, size_t tam);
 int quiPrimListDobCirc(tListaDobCirc *pLista, void *dato, size_t tam);
+//Operaciones al final
+int insUltListDobCirc(tListaDobCirc *pLista, void *dato, size_t tam);
+int quiUltListDobCirc(tListaDobCirc *pLista, void *dato, size_t tam);
+//Operaciones de consulta
+int verPrimListDobCirc(const tListaDobCirc *pLista, void *dato, size_t tam);
+int verUltListDobCirc(const tListaDobCirc *pLista, void *dato,size_t tam);
+//Operaciones de ordenamiento
+int ordListDobCirc(tListaDobCirc *pLista, int (*comparar)(const void *, const void *));
+//Operaciones de eliminación
+void vaciarListDobCirc(tListaDobCirc *pLista);
+int elimListDobCirc(tListaDobCirc *pLista, void *dato, size_t tam, int (*comparar)(const void *, const void *));
 #endif // LISTADOENCI_H_INCLUDED
